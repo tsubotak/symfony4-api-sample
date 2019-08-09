e129574f8dbf2765095b47b33ef4c581d069bfbf
tsubota <tsubotak@eli-sys.jp>
2019-08-09 15:09:00 +0900

    fix unit test

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -18,6 +18,6 @@ class ProductTest extends TestCase
     public function testName()
     {
         $this->product->setName('someName');
-        $this->assertSame('invalidName', $this->product->getName());
+        $this->assertSame('someName', $this->product->getName());
     }
 }
